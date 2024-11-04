0ed8f4f18febac72c072bdba8894c27e34e06d0c
codingcatMike <gpt0Mike@gmail.com>
2024-11-04 21:25:52 +0100

    normal change

--- Demokratietag_Derksen/models.py
+++ Demokratietag_Derksen/models.py
@@ -2,11 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    surname = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    exit_time = models.TimeField(null=True, blank=True)
-    entry_time = models.TimeField(null=True, blank=True)
-    verbleibende_zeit = models.CharField(max_length=255)
-    person_id = models.IntegerField(null=True)
+    surname = models.CharField(max_length=255, default='')
+    name = models.CharField(max_length=255, default='')
+    exit_time = models.TimeField(null=True, blank=True, default=None)
+    entry_time = models.TimeField(null=True, blank=True, default=None)
+    verbleibende_zeit = models.CharField(max_length=255, default='')
+    person_id = models.IntegerField(null=True, default=None)
+    klasse = models.CharField(max_length=255, default='')
+    lehrer = models.CharField(max_length=255, default='')
+    raum = models.CharField(max_length=255, default='')
+    projekt = models.CharField(max_length=255, default='')
+    anreise = models.CharField(max_length=255, default='')
+    thiersee = models.CharField(max_length=255, default='')
+    dinge = models.CharField(max_length=255, default='')
+    brotzeit = models.CharField(max_length=255, default='')
+    handy = models.CharField(max_length=255, default='')
+    anzahl = models.IntegerField(null=True, default=None)
+    getraenk = models.CharField(max_length=255, default='')
+    completename = models.CharField(max_length=255, default='')
+    in_or_out =  models.CharField(max_length=3, default='')
+    times_in =  models.IntegerField(null=True, default=None)
 
-    
--- Demokratietag_Derksen/views.py
+++ Demokratietag_Derksen/views.py
@@ -1,111 +1,163 @@
-from django.shortcuts import render
-from datetime import datetime
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .models import Person
-from datetime import datetime, timedelta
-from datetime import date
-from django.http import HttpResponse
+from datetime import date, datetime, timedelta
 from zoneinfo import ZoneInfo
+
+from django.contrib import messages
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from .models import Person
+
 berlin_time = None
 from django.shortcuts import redirect
+
 from .models import *
 
 
 def ShowPers(request):
-    return render(request, 'Pers.html')
-
-
-
+    return render(request, "Pers.html")
 
 
 def startpage(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def ShowPers(request):
     person = Person.objects.all()
-    return render(request, 'PersShow.html', {'person' : person})
+    return render(request, "PersShow.html", {"person": person})
+
 
 def check(request):
     person = Person.objects.all()
-    return render(request, 'check.html', {'Person' : person})
+    return render(request, "check.html", {"Person": person})
+
 
 def delete_database(request):
     Person.objects.all().delete()
-    messages.success(request, 'Database deleted successfully')
-    return redirect('startpage')
+    messages.success(request, "Database deleted successfully")
+    return redirect("startpage")
+
 
 def show(request):
-    if request.method  == 'POST':
-        name = request.POST.get('name')
-        surename = request.POST.get('surename')
-        id = request.POST.get('person_id')
-        Person.objects.filter(person_id = id).delete()
-        print('The Person  with id: ', id, ' has been deleted')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        surename = request.POST.get("surename")
+        id = request.POST.get("person_id")
+        Person.objects.filter(person_id=id).update(in_or_out="out")
+        print("The Person  with id: ", id, " has been outed")
 
-    person = Person.objects.all()
-    return render(request, 'show.html', {'person': person})
+    person = Person.objects.filter(in_or_out="in").order_by("surname", "name")
+    return render(request, "show.html", {"person": person})
+
 
 from django.http import JsonResponse
 
+
 def update_item(request):
-    berlin_tz = ZoneInfo('Europe/Berlin')
+    berlin_tz = ZoneInfo("Europe/Berlin")
     berlin_time = datetime.now(berlin_tz)
 
     updated = False
     for person in Person.objects.all():
         exit_time = datetime.combine(date.today(), person.exit_time)
         exit_time = exit_time.replace(tzinfo=berlin_tz)  # Make exit_time offset-aware
-        
-        berlin_time = berlin_time.replace(second=round(berlin_time.second), microsecond=0)
+
+        berlin_time = berlin_time.replace(
+            second=round(berlin_time.second), microsecond=0
+        )
         if exit_time > berlin_time:
             verbleibende_Zeit = exit_time - berlin_time
         else:
             vor_ver_Zeit = berlin_time - exit_time
-            verbleibende_Zeit = '-' + str(vor_ver_Zeit)
-        
+            verbleibende_Zeit = "-" + str(vor_ver_Zeit)
+
         if person.verbleibende_zeit != str(verbleibende_Zeit):
             person.verbleibende_zeit = str(verbleibende_Zeit)
             person.save()
             updated = True
-    
-    return JsonResponse({'updated': updated})
+
+    return JsonResponse({"updated": updated})
+
 
 def add(request):
-    berlin_tz = ZoneInfo('Europe/Berlin')
+    berlin_tz = ZoneInfo("Europe/Berlin")
     berlin_time = datetime.now(berlin_tz)
 
-    if Person.objects.exists():
-        max_id = Person.objects.all().order_by('-person_id')[0].person_id
-        max = max_id + 1
-        print(max_id)
-        print(max)
-    else:
-        max = 0
+    if request.method == "POST":
+        surname = request.POST.get("surname")
+        name = request.POST.get("name")
+        exit_hours = int(request.POST.get("exit_hours"))
+        exit_minutes = int(request.POST.get("exit_minutes"))
+        klasse = request.POST.get("klasse")
+        lehrer = request.POST.get("lehrer")
+        raum = request.POST.get("raum")
+        projekt = request.POST.get("projekt")
+        anreise = request.POST.get("anreise")
+        thiersee = request.POST.get("thiersee")
+        dinge = request.POST.get("dinge")
+        brotzeit = request.POST.get("brotzeit")
+        handy = request.POST.get("handy")
+        anzahl = request.POST.get("anzahl")
+        getraenk = request.POST.get("getraenk")
+        completename = request.POST.get("completename")
+        person_id = request.POST.get("person_id")
 
-    if request.method == 'POST':
-        surname = request.POST.get('surname')
-        name = request.POST.get('name')
-        exit_hours = int(request.POST.get('exit_hours'))
-        exit_minutes = int(request.POST.get('exit_minutes'))
-        completename = request.POST.get('completename')  
-        
-        berlin_time = berlin_time.replace(second=round(berlin_time.second), microsecond=0)
+        berlin_time = berlin_time.replace(
+            second=round(berlin_time.second), microsecond=0
+        )
         exit_time = berlin_time.replace(hour=exit_hours, minute=exit_minutes, second=0)
-  
+
         print(berlin_time)
         print(exit_time)
         verbleibende_zeit = exit_time - berlin_time
         if verbleibende_zeit.total_seconds() < 0:
             verbleibende_zeit = timedelta(days=1) + verbleibende_zeit
             print(verbleibende_zeit)
+
         hours, remainder = divmod(verbleibende_zeit.seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
         verbleibende_zeit_str = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
         print(berlin_time)
         print(verbleibende_zeit)
-        person = Person(surname=surname, name=name, exit_time=exit_time, entry_time=berlin_time, verbleibende_zeit=str(verbleibende_zeit), person_id=max)
-        person.save()
-        messages.success(request, 'Person added successfully')
-    return render(request, 'add.html')
-        
+        if person_id == "":
+            person_id_1 = Person.objects.all().order_by('-person_id').first().person_id + 1 if Person.objects.all() else 1
+            person = Person(
+                surname=surname,
+                name=name,
+                exit_time=exit_time,
+                entry_time=berlin_time,
+                verbleibende_zeit=str(verbleibende_zeit),
+                person_id=person_id_1,
+                klasse=klasse,
+                lehrer=lehrer,
+                raum=raum,
+                projekt=projekt,
+                anreise=anreise,
+                thiersee=thiersee,
+                dinge=dinge,
+                brotzeit=brotzeit,
+                handy=handy,
+                anzahl=anzahl,
+                getraenk=getraenk,
+                completename=completename,
+                in_or_out="in",
+                times_in=1,
+                
+            )
+            person.save()
+
+            existing_person = Person.objects.filter(person_id=person_id).first()
+        else:
+            if existing_person:
+                # Wenn die person_id existiert, setze in_or_out auf "in"
+                existing_person.in_or_out = "in"
+                existing_person.exit_time = exit_time
+                existing_person.entry_time = berlin_time
+                existing_person.verbleibende_zeit = str(verbleibende_zeit)
+                existing_person.times_in = existing_person.times_in + 1
+
+                existing_person.save()  # Speichern der Änderungen
+                messages.success(request, "Person updated successfully")
+      
+            
+
+    return render(request, "add.html")
